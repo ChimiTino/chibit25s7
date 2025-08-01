6fe5e8accaca885885a11f6fafe3eae553acdb8d
ChimiTino <86851384+ChimiTino@users.noreply.github.com>
2025-08-01 01:11:04 -0400

    first part of the main.js done

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -4,15 +4,12 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const imageList = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+const altText = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': 'Section of wall from a pharaoh\'s tomb',
+  'pic5.jpg': 'Large moth on a leaf'
+};
